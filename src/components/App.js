4a7f02f1ad2768c4d51c119b76d4ef37042da4db
ShriGanesh <rvsaini8107@gmail.com>
2023-07-14 15:49:01 +0530

    submitting code-Fri Jul 14 2023 15:48:55 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
+import React,{useState} from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
 
-const App = () => {
+function App() {
+  const tabs = [
+    { title: "Tab 1", content: "This is the content for Tab 1." },
+    { title: "Tab 2", content: "This is the content for Tab 2." },
+    { title: "Tab 3", content: "This is the content for Tab 3." },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tabs tabs={tabs} />
     </div>
-  )
+  );
 }
+function Tabs(props) {
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-export default App
+  const handleTabClick = (index) => {
+    setActiveTabIndex(index);
+  };
+
+  return (
+    <div>
+      <ul>
+        {props.tabs.map((tab, index) => (
+          <li key={index} onClick={() => handleTabClick(index)}>
+            {tab.title}
+          </li>
+        ))}
+      </ul>
+      <div>{props.tabs[activeTabIndex].content}</div>
+    </div>
+  );
+}
+export default App;
