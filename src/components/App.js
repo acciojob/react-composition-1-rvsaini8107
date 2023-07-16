6f4d3c6bed4fda394c7be9aaaef57934352dd8ae
ShriGanesh <rvsaini8107@gmail.com>
2023-07-16 13:55:36 +0530

    submitting code-Sun Jul 16 2023 13:55:33 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,36 +3,14 @@ import "./../styles/App.css";
 
 
 function App() {
-  const tabs = [
-    { title: "Tab 1", content: "This is the content for Tab 1." },
-    { title: "Tab 2", content: "This is the content for Tab 2." },
-    { title: "Tab 3", content: "This is the content for Tab 3." },
-  ];
-
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <Tabs tabs={tabs} />
+      <p>Button clicked {count} times</p>
+      <button onClick={()=>setCount(count+1)}>Click me</button>
     </div>
-  );
-}
-function Tabs(props) {
-  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  )
 
-  const handleTabClick = (index) => {
-    setActiveTabIndex(index);
-  };
 
-  return (
-    <div>
-      <ul>
-        {props.tabs.map((tab, index) => (
-          <li key={index} onClick={() => handleTabClick(index)}>
-            {tab.title}
-          </li>
-        ))}
-      </ul>
-      <div>{props.tabs[activeTabIndex].content}</div>
-    </div>
-  );
 }
 export default App;
